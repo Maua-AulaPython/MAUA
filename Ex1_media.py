2725c8cc3317900d1863a126eb3149e0a3125bc9
alanbarredo <alan_barredo@hotmail.com>
2015-03-20 14:53:25 -0300

    Create Ex1_media.py

---
+++ Ex1_media.py
@@ -0,0 +1,4 @@
+Python 2.7 . 9 (padrão, Dec 10  2014 , 12 : 24 : 55 ) [MSC v.1500 32 bits (Intel)] em win32
+Escreva " copyright " , " credits "  ou  " licença () "  para obter mais informações.
+>>>  def meios (a, b, c):
+    retorno ( flutuador (a) + flutuador (b) + flutuador (c)) / 3
