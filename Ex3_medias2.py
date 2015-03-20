5a977923193c10a2f197e5da57696bcd4c29f7a0
alanbarredo <alan_barredo@hotmail.com>
2015-03-20 14:57:02 -0300

    Create Ex3_medias2.py

---
+++ Ex3_medias2.py
@@ -0,0 +1,16 @@
+a = 0
+soma = 0
+media = 0
+b = 1
+enquanto b> 0:
+  c = float (raw_input ("nota Uma digite:"))
+  se c> 0:
+      a = a + 1
+      Soma = c + soma
+  else:
+      b = 0
+      imprimir "a mídia e:"
+
+media = (soma / a)
+mídia impressa
+
